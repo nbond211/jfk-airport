bbebf333f027211f067d706d93bf9d6382b15be1
nbond211 <nbond211@verizon.net>
2016-11-03 15:44:00 -0400

    filtering

--- js/app.js
+++ js/app.js
@@ -15,11 +15,44 @@ jfkApp.controller('LanguageController', function LanguageController($scope) {
 
 // Define the `FlightController` controller
 jfkApp.controller('FlightController', function FlightController($scope) {
-    $scope.selectedLang = "english";
+    $scope.flights = [
+        {   "flightNumber": 322, 
+            "airline": "Delta",
+            "location": "Atlanta, GA", 
+            "terminal": "B", 
+            "gate": 22, 
+            "status": "On-Time",
+            "scheduled": "4:25 PM",
+        },
+        {   "flightNumber": 5733, 
+            "airline": "American Airlines",
+            "location": "Boston, MA", 
+            "terminal": "G", 
+            "gate": 3, 
+            "status": "On-Time",
+            "scheduled": "7:25 PM",
+        },
+    ];
     
-    $scope.selectLang = function(newLang) {
-        $scope.selectedLang = newLang;
-    };
+    $scope.flightType = "arrivals";
+    
+    $scope.airlines = ["Delta", "American Airlines", "Jet Blue"];
+    
+    $scope.filterFlight = function(flight) {
+        if ($scope.airline == null && $scope.flighNumber == null) {
+            return true;
+        }
+        if ($scope.airline == null) {
+            return flight.flightNumber == $scope.flightNumber;
+        }
+        if ($scope.flightNumber == null) {
+            return flight.airline == $scope.airline;
+        }
+        else {
+            return flight.flightNumber == $scope.flightNumber && flight.airline == $scope.airline;
+        }
+    }
+    
     
     
 });
