ed70ec6959f34106f85d4c153a31e79c32a80167
nbond211 <nbond211@verizon.net>
2016-11-03 20:07:03 -0400

    car rental page

--- js/app.js
+++ js/app.js
@@ -8,8 +8,6 @@ jfkApp.controller('LanguageController', function LanguageController($scope) {
     $scope.selectLang = function (newLang) {
         $scope.selectedLang = newLang;
     };
-
-
 });
 
 
@@ -60,5 +58,116 @@ jfkApp.controller('FlightController', function FlightController($scope, $http) {
     };
 
 
-
 });
+
+
+// Define the `CarController` controller
+jfkApp.controller('CarController', function CarController($scope) {
+    $scope.pickupLocation = "JFK International Airport";
+    $scope.dropoffLocation = "JFK International Airport";
+    
+    $scope.carTypes = ["Compact", "Sedan", "Mid-size", "SUV", "Truck"];
+    
+    $scope.filterCar = function(car) {
+        if ($scope.pickupDate == undefined && $scope.carType == undefined) {
+            return true;
+        }
+        if ($scope.pickupDate == undefined) {
+            return car.type == $scope.carType;
+        }
+        if ($scope.carType == undefined) {
+            console.log(car.available);
+            console.log($scope.pickupDate);
+            return car.available.toDateString() == $scope.pickupDate.toDateString();
+        }
+        else {
+            console.log(car.available);
+            console.log($scope.pickupDate);
+            return car.type == $scope.carType && car.available.toDateString() == $scope.pickupDate.toDateString();
+        }
+    };
+    
+    $scope.cars = [
+        {
+            name: "Honda Civic", 
+            type: "Compact", 
+            available: new Date("2017-01-05T05:00:00.000Z"), // 1/5/16
+            image: "http://st.motortrend.com/uploads/sites/10/2016/10/2017-Honda-Civic-sedan-front-three-quarter.jpg",
+            selected: false
+        },
+        {
+            name: "Ford Focus", 
+            type: "Compact", 
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "https://carsintrend.com/wp-content/uploads/2016/01/2017-Ford-Focus.jpg",
+            selected: false
+        },
+        {
+            name: "Mazda3", 
+            type: "Compact", 
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "http://cms.kelleybluebookimages.com/content/dam/kbb-editorial/make/mazda/mazda3/2017/05-2017%20-mazda-mazda3.jpg",
+            selected: false
+        },
+        {
+            name: "Toyota Camry", 
+            type: "Sedan", 
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "http://carpreview.com/wp-content/uploads/2015/11/pic-1-1.jpg",
+            selected: false
+        },
+        {
+            name: "Chevrolet Impala", 
+            type: "Sedan",
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "https://carsintrend.com/wp-content/uploads/2016/01/2017-Chevrolet-Impala-release-2-1.jpg",
+            selected: false
+        },
+        {
+            name: "Buick Verano", 
+            type: "Mid-size", 
+            available: new Date("2017-01-08T05:00:00.000Z"), 
+            image: "http://gmauthority.com/blog/wp-content/gallery/2016-buick-verano-chinese-market/2016-buick-verano-china-01.jpg",
+            selected: false
+        },
+        {
+            name: "Ford Explorer", 
+            type: "SUV", 
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "http://www.ford.com/ngbs-services/resources/ford/explorer/2017/equipment/exp17_models_detailflip_platinumgrille.jpg",
+            selected: false
+        },
+        {
+            name: "Land Rover Range Rover", 
+            type: "SUV", 
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "https://a.gaw.to/photos/8/2/82154_2017_landrover_Range_Rover.jpg?460x287",
+            selected: false
+        },
+        {
+            name: "Jeep Wrangler", 
+            type: "SUV", 
+            available: new Date("2017-01-05T05:00:00.000Z"), 
+            image: "http://carpreview.com/wp-content/uploads/2016/05/pic-1-4.jpg",
+            selected: false
+        },
+        {
+            name: "Nissan Titan", 
+            type: "Truck", 
+            available: new Date("2017-01-08T05:00:00.000Z"), 
+            image: "http://assets.nydailynews.com/polopoly_fs/1.2751170.1471277640!/img/httpImage/image.jpg_gen/derivatives/article_750/2017-nissan-titan.jpg",
+            selected: false
+        }
+    ];
+    
+});
+
+
+
+
+
+
+
+
+
+
